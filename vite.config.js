fc28bd9b8fdb83353e47b97d0cab18bc79cdd585
juanrubilink <juan@rubilink.io>
2023-02-22 09:27:09 +0900

    fourth commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: 'src/App.jsx'
+    }
+  }
 })
